110e553ff6703911e020eeccee62ef20f10678c1
leeho0604 <ejaosaos@naver.com>
2017-08-21 01:18:33 +0000

    Update file_client_cctv.cpp

--- file_client_cctv.cpp
+++ file_client_cctv.cpp
@@ -9,19 +9,38 @@
 #include <mysql.h>
 #include <iostream>
 
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+//추가
+#include <zmq.h>
+#include <assert.h>
+#include <sys/types.h>
 
 class Data{
 	public: 
 	char unique_key[100];
 	char image_addr[200];
 };
+class Cctv_data
+{
+	public: 
+		char cctv_id [5];
+		char ip [20];
+};
+class mbuf {
+	public :
+	long mtype;
+	char buf[100];
+	char unique_key[100];
+	char image_addr[200];
+};
 
 #define PORT 9002
 #define IPADDR "192.168.1.49"
-
 #define BUFSIZE 1024
-
 #define ARG_MAX 6
+const int type = 1;
 const char *host = (char*)"localhost";
 const char *user = (char*)"root";
 const char *pw = (char*)"bitiotansehen";
@@ -50,27 +69,27 @@ int search_file(const char *filename)
 	return (access(filename, F_OK) == 0);
 }
 void dataToCCTV(char *unique_key, char * filename);
+void SendMsgToCCTV(char *unique_key, char * filename);
+
 main( )
 {
 	int	c_socket;
 	struct sockaddr_in c_addr;
 	int	len;
 	int	n;
+        char query[BUFSIZ];
+	char *ptr;
+	char c_buff[ARG_MAX][BUFSIZ];
+	char cctv_id[5];
+	char ip_add[20];
 
 	MYSQL *connection;
         MYSQL_RES  *sql_result;
         MYSQL_ROW sql_row;
+	Data data;
 
-        char query[BUFSIZ];
-	char *ptr;
-	char c_buff[ARG_MAX][BUFSIZ];
-	char	rcvBuffer[BUFSIZ];
-	
-	char cctv_id[5];
-	char ip_add[20];
 
-//PF_INET , AF_INET 차이는 ???
-
+	//socket connect
 	c_socket = socket(AF_INET, SOCK_STREAM, 0);
 	
 	memset(&c_addr, 0, sizeof(c_addr));
@@ -80,16 +99,15 @@ main( )
 	
 	if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr))<0) {
 		printf("Can not connect\n");
-		close(c_socket);
+	  	close(c_socket);
 		return -1;
 	}
-
-	
 	socklen_t addrlen;
 	char buf[BUFSIZE];
 
 
 //mysql 에서 cctv 정보 읽어오기
+
 	connection = mysql_init(NULL);
         if(!mysql_real_connect(connection,host,user,pw,db,0,NULL,0))
         {
@@ -97,9 +115,7 @@ main( )
 		exit(1);
         }
 
-        	//send sql query
-
-
+        //send sql query
         if(mysql_query(connection, "select * from CCTV_INFO"))
         {
                 fprintf(stderr,"%s\n",mysql_error(connection));
@@ -107,7 +123,7 @@ main( )
         }
         sql_result = mysql_use_result(connection);
 
-        	//output table name
+        //output table name
         printf("MySQL Tables in mysql database:\n");
         while((sql_row=mysql_fetch_row(sql_result))!=NULL)
         {        
@@ -116,142 +132,169 @@ main( )
 		printf("ip_address : %s\n",sql_row[2]);
 		strcpy(ip_add, sql_row[2]);
 	}
-
         mysql_free_result(sql_result);
 
-// send 넣어주기
+	
+	// send 넣어주기
+	
+	Cctv_data cctv_data;
+	strcpy(cctv_data.cctv_id,cctv_id);
+	strcpy(cctv_data.ip,ip_add);
 
-//	send(c_socket, cctv_id, strlen(cctv_id)+1, 0);
-//	send(c_socket, ip_add, strlen(ip_add)+1, 0);
-//	printf("ip_address : %s\n",ip_add);
-
-/////////////////////// 
-
-
-
+	send(c_socket, &cctv_data, sizeof(cctv_data), 0);
+	//send(c_socket, ip_add, strlen(ip_add)+1, 0);
+	printf("[send] cctv_id : %s \nip_address : %s\n",cctv_data.cctv_id, cctv_data.ip);
+	
 
 // file ( while 문 안에 넣어 주기 )
 
-
-	// 파일 이름 받기
-	/*
+	while(1)
+	{
+		int TT;
+		recv(c_socket, &TT, sizeof(TT),MSG_WAITALL);
+		//printf("TT : %d\n",TT);
+		
+		if(TT==1)
+		{
+			//class로 data 받기
+			Data data;
+			char unique_key[100];
+			char filename[256];
+			
+			bzero(&data, sizeof(data));
 	
-	int retval;
-	memset(filename, 0, sizeof(filename));
-	*/
-	Data data;
-	char unique_key[100];
-	char filename[256];
-
-	int retval = recv(c_socket, &data, sizeof(data),0);
-	
-
-	cout<<"after recv"<<endl;
-
-	if(retval < 0){
-		err_display("recv()");
-		close(c_socket);
-//		continue;
-	}
-
-	/*
-	filename[retval] = '\0';
-	printf("-> 받을 파일 이름: %s\n", filename);
-	*/
-	
-	printf("%s %s\n", data.unique_key, data.image_addr);
-	strcpy(filename, data.image_addr);
-	strcpy(rcvBuffer, data.unique_key);
-
-	// 파일 검색
-	int currbytes = 0;
-	if(search_file(filename)){
-		// 현재의 파일 크기 얻기
-		FILE *fp = fopen(filename, "rb");
-		if(fp == NULL){
-			perror("파일 입출력 오류");
-			close(c_socket);
-	//		continue;
-		}
-		fseek(fp, 0, SEEK_END);
-		currbytes = ftell(fp);
-		fclose(fp);
-	}
-
-	// 전송을 시작할 위치(=현재의 파일 크기) 보내기 (이어받기)
-	retval = send(c_socket, (char *)&currbytes, sizeof(currbytes), 0);
-	if(retval < 0){
-		err_display("send()");
-		close(c_socket);
-	//	continue;
-	}
-
-	// 전송 받을 데이터 크기 받기
-	int totalbytes;
-	retval = recv(c_socket, (char *)&totalbytes, sizeof(totalbytes), 0);
-	if(retval < 0){
-		err_display("recv()");
-		close(c_socket);
-	//	continue;
-	}
-		printf("-> 전송 받을 데이터 크기: %d 바이트 중 %d 바이트\n"
-		       "                         (%d 바이트는 이미 받음)\n",
-			currbytes+totalbytes, totalbytes, currbytes);
-
-		// 파일 열기
-		FILE *fp = fopen(filename, "ab"); // append & binary mode
-		if(fp == NULL){
-			perror("파일 입출력 오류");
-			close(c_socket);
-	//		continue;
-		}
-
-		// 파일 데이터 받기
-		int numtotal = 0;
-		while(1){
-			retval = recv(c_socket, buf, BUFSIZE, MSG_WAITALL);
-			if(retval > 0){	
-				fwrite(buf, 1, retval, fp);
-				if(ferror(fp)){
-					perror("파일 입출력 오류");
-					break;
-				}
-				numtotal += retval;
-			}
-			else if(retval == 0){
-				break;
+			int retval = recv(c_socket, &data, sizeof(data),MSG_WAITALL);
+			if(retval < 0){
+				err_display("recv()");
+				close(c_socket);
+				continue;
 			}
 			else{
-				err_display("recv()");
-				break;
+				printf("got data\n");
 			}
-		}
-		fclose(fp);
-
-		// 전송 결과 출력
-		if(numtotal == totalbytes)
-		{
-			printf("-> 파일 전송 완료!\n");
+			strcpy(filename, data.image_addr);
+			strcpy(unique_key, data.unique_key);
+		
+			// 파일 검색
+			int currbytes = 0;
+			if(search_file(filename)){
+				// 현재의 파일 크기 얻기
+				FILE *fp = fopen(filename, "rb");
+				if(fp == NULL){
+					perror("파일 입출력 오류");
+					close(c_socket);
+					continue;
+				}
+				fseek(fp, 0, SEEK_END);
+				currbytes = ftell(fp);
+				fclose(fp);
+			}
+	
+			// 전송을 시작할 위치(=현재의 파일 크기) 보내기 (이어받기)
+			retval = send(c_socket, (char *)&currbytes, sizeof(currbytes), 0);
+			if(retval < 0){
+				err_display("send()");
+				close(c_socket);
+				continue;
+			}
+	
+			// 전송 받을 데이터 크기 받기
+			int totalbytes;
+			retval = recv(c_socket, (char *)&totalbytes, sizeof(totalbytes), 0);
+			if(retval < 0){
+				err_display("recv()");
+				close(c_socket);
+				continue;
+			}
+			printf("-> 전송 받을 데이터 크기: %d 바이트 중 %d 바이트\n"
+				       "                         (%d 바이트는 이미 받음)\n",
+					currbytes+totalbytes, totalbytes, currbytes);
+	
+			// 파일 열기
+			FILE *fp = fopen(filename, "ab"); // append & binary mode
+			if(fp == NULL){
+				perror("파일 입출력 오류");
+				close(c_socket);
+				continue;
+			}
+	
+			// 파일 데이터 받기
+			int numtotal = 0;
+			while(1){
+				if(numtotal == totalbytes){
+					break;
+				}
+				retval = recv(c_socket, buf, BUFSIZE, 0);
+				printf("retval : %d\n",retval);
+				if(retval > 0){	
+					fwrite(buf, 1, retval, fp);
+					if(ferror(fp)){
+						perror("파일 입출력 오류");
+						break;
+					}
+				numtotal += retval;
+				}
+				else if(retval == 0){
+					break;
+				}
+				else{
+					err_display("recv()");
+					break;
+				}
+			}
+		
+	//intercommunication between python and c++ using zmq
+	
+		/*	void *context = zmq_ctx_new ();
+			void *responder = zmq_socket (context, ZMQ_REP);
+			void *respond = zmq_socket (context, ZMQ_REP);
+			int rc = zmq_bind (responder, "tcp://*:5555");
+			//assert (rc == 0);    
+			char buffer [40]={0,},sbuff [40]={0,}, rbuff [40]={0,};
 			
+			zmq_recv (responder, buffer, 20, 0);
+			printf("python3 받음 buffer: %s\n",buffer);
+		
+			//문자열 받기,  filename
+			strcpy( rbuff, data.image_addr);
+		        printf("파일이름 받기rbuff : %s\n",rbuff);
+			
+			usleep (100);          //  Do some 'work'
+		        
+			//문자열 보내, filename
+			snprintf(sbuff,sizeof(sbuff), "%s", rbuff);
+		        printf ("filename 보내:<%s>\n", sbuff);
+		        zmq_send (responder, sbuff, strlen(sbuff), 0);
+		 */
+		    
+			fclose(fp);
+		
+			// 전송 결과 출력
+			if(numtotal == totalbytes)
+			{
+				printf("-> 파일 전송 완료!\n");
+				dataToCCTV(unique_key, filename);
+				SendMsgToCCTV(unique_key, filename);
+			}
+			else
+			{
+				printf("-> 파일 전송 실패!\n");
+			}
+
 		}
 		else
-			printf("-> 파일 전송 실패!\n");
+		{	printf("TT==1아닐때\n");
+		
+		
+			char unique_key[100];
 
-
-	/// unique key
-	/*
-	if((n = read(c_socket, rcvBuffer, sizeof(rcvBuffer))) < 0) {
-		return (-1);
+	
+			int retval = recv(c_socket, unique_key, sizeof(unique_key),MSG_WAITALL);
+			printf("unique_key_beacon signal : %s\n",unique_key);
+			
+			
+		}
 	}
-	
-	rcvBuffer[n] = '\0';*/
-
-	printf("received Data : %s\n", rcvBuffer);
-	dataToCCTV(rcvBuffer, filename);
-
-
-
-	
 	close(c_socket);
 }
 void dataToCCTV(char *unique_key, char * image_add)
@@ -278,9 +321,16 @@ void dataToCCTV(char *unique_key, char * image_add)
                 exit(1);
         }
       
-	
-
 	printf("unique_key : %s\n", unique_key);
 	printf("image_add : %s\n", image_add);
+}
+void SendMsgToCCTV(char *t_unique_key, char * t_image_add)
+{
+	int msgid = msgget(1234, IPC_CREAT);
 
+	mbuf msg;
+	msg.mtype = type;
+	strcpy(msg.unique_key,t_unique_key);
+	strcpy(msg.image_addr, t_image_add);
+	msgsnd(msgid, (void*)&msg, sizeof(mbuf), 0);
 }
