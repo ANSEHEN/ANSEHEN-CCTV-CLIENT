f774eea67ec8c055bf4e8687ad5e668a7b6ccd74
sungkyun88 <tasung08@gmail.com>
2017-09-04 01:01:24 +0000

    Update detect_cctv.cpp

--- detect_cctv.cpp
+++ detect_cctv.cpp
@@ -5,18 +5,12 @@
 #include <assert.h>
 #include <sys/types.h>
 
-class Data{
-	public:
-	char unique_key[100];
-	char image_addr[200];
-};
+
 void crop_thread1()
 {
 
 	printf("[crop]\n");
-	char t_string[2][10] = {"0_4","1_2"};// (검색 회차 _ 개수)
-	
-	Data data;
+	char t_string[10] = {"0_4"};// (검색 회차 _ 개수)
 	
 	void *context = zmq_ctx_new();
 	void *responder = zmq_socket (context, ZMQ_REQ);
@@ -25,44 +19,17 @@ void crop_thread1()
 	for(int i=0 ; i<1;i++)
 	{
 		char buffer [40] = {0,}, sbuff[40] = {0,} ,rbuff[40]={0,};
-		strcpy(data.image_addr, t_string[i]);
-		strcpy(rbuff, data.image_addr);
-		printf("rbuff: %s\n", rbuff);
+		
+		strcpy(sbuff, t_string);
+
 		usleep(100);
-		snprintf(sbuff, sizeof(sbuff), "%s", rbuff);
 		printf("filename send : [ %s ]\n", sbuff);
 		zmq_send (responder, sbuff, strlen(sbuff), 0);
 		zmq_recv (responder, buffer, 20, 0);
-		printf(" %s\n",buffer);
-	}
-}
-void crop_thread2()
-{
-
-	printf("[crop]\n");
-	char t_string[2][10] = {"0_4","1_2"};
 	
-	Data data;
-	
-	void *context = zmq_ctx_new();
-	void *responder = zmq_socket (context, ZMQ_REQ);
-	int rc = zmq_bind(responder, "tcp://*:5560");
-
-	for(int i=0; i<2;i++)
-	{
-		char buffer [40] = {0,}, sbuff[40] = {0,} ,rbuff[40]={0,};
-		strcpy(data.image_addr, t_string[i]);
-		strcpy(rbuff, data.image_addr);
-		printf("rbuff: %s\n", rbuff);
-		usleep(100);
-		snprintf(sbuff, sizeof(sbuff), "%s", rbuff);
-		printf("filename send : [ %s ]\n", sbuff);
-		zmq_send (responder, sbuff, strlen(sbuff), 0);
-		zmq_recv (responder, buffer, 20, 0);
-		printf(" %s\n",buffer);
 	}
 }
 int main(void)
 {
-	crop_thread2();
+	crop_thread1();
 }
